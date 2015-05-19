02ac4b2b1f290f15ee60fb11e4288de51ccf313d
marekczuma <marek.czuma@gmail.com>
2015-05-19 19:47:24 +0200

    I dont know

---
+++ ElonsRiot/ElonsRiot/ElonsRiot/BoxMovementAI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElonsRiot
+{
+    class BoxMovementAI
+    {
+        public GameObject PointB { get; set; }      // Punkt w którym ma się postać zatrzymać
+        public GameObject PointC { get; set; }      // Punkt do którego ma spitolić
+        public bool BIncluded { get; set; }         // Czy byłeś już w B?
+        public bool CIncluded { get; set; }         // Czy byłeś już w C?
+
+        public BoxMovementAI(GameObject _b, GameObject _c)
+        {
+            PointB = _b;
+            PointC = _c;
+            BIncluded = false;
+            CIncluded = false;
+        }
+    }
+}
