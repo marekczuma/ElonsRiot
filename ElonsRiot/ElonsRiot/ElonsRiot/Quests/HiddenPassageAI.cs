3411a7020b6788a15cdc13488798ead3ab573d67
marekczuma <marek.czuma@gmail.com>
2015-06-22 23:22:10 +0200

    Hidden Passage Quest works

--- ElonsRiot/ElonsRiot/ElonsRiot/Quests/HiddenPassageAI.cs
+++ ElonsRiot/ElonsRiot/ElonsRiot/Quests/HiddenPassageAI.cs
@@ -6,9 +6,61 @@ using System.Text;
 
 namespace ElonsRiot.Quests
 {
-    class HiddenPassageAI
+    public class HiddenPassageAI
     {
         public Vector3 PosButtonA { get; set; }
         public Vector3 PosButtonB { get; set; }
+
+        public bool AIsIncluded { get; set; }
+        public bool BIsIncluded { get; set; }
+        public bool WallIsMoving { get; set; }
+        public bool QuestFinished { get; set; }
+        
+        public GameObject WallTarget { get; set; }
+
+        public Scene Scene { get; set; }
+
+        private float timer = 0;
+
+        public HiddenPassageAI()
+        {
+            AIsIncluded = false;
+            BIsIncluded = false;
+            QuestFinished = false;
+            WallIsMoving = false;
+        }
+
+        public void CheckIsIncluded()
+        {
+            if(timer <=0)
+            {
+                foreach (var elem in Scene.GameObjects)
+                {
+                    if (Vector3.Distance(elem.Position, PosButtonA) <= 5)
+                    {
+                        AIsIncluded = true;
+                        break;
+                    }else
+                    {
+                        AIsIncluded = false;
+                    }
+                }
+                foreach (var elem in Scene.GameObjects)
+                {
+                    if (Vector3.Distance(elem.Position, PosButtonB) <= 5)
+                    {
+                        BIsIncluded = true;
+                        break;
+                    }else
+                    {
+                        BIsIncluded = false;
+                    }
+                }
+                timer = 1000;
+            }
+            float timeInMS = Scene.time.ElapsedGameTime.Milliseconds;
+            timer -= timeInMS;
+        }
+
     }
 }
