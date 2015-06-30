1df8e846de93f135ebf4d49ab5a620e659f553e2
marekczuma <marek.czuma@gmail.com>
2015-06-30 03:39:04 +0200

    Nowy intro screen

---
+++ ElonsRiot/ElonsRiot/ElonsRiot/DialogTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ElonsRiot
+{
+     [Serializable]
+    public class DialogTime
+    {
+        [XmlElement("time")]
+        public List<float> Times { get; set; }
+    }
+}
